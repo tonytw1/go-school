9da495c66322f798a96568635e2ad06fa7202a6a
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-11-09 17:11:26 +0000

    Pick up card actions; null strings behaving list empties?

--- api/trello.go
+++ api/trello.go
@@ -24,7 +24,26 @@ type TrelloCard struct {
 	DateLastActivity time.Time
 }
 
+type TrelloActionList struct {
+	Id   string
+	Name string
+}
+
+type TrelloActionData struct {
+	ListBefore TrelloActionList
+	ListAfter  TrelloActionList
+}
+
+type TrelloAction struct {
+	Id   string
+	Date time.Time
+	Type string
+	Data TrelloActionData
+}
+
 const trello_api = "https://api.trello.com/1"
+const key = ""
+const token = ""
 
 func GetCards() ([]TrelloCard, error) {
 	u, e := url.Parse(trello_api + "/search")
@@ -33,8 +52,8 @@ func GetCards() ([]TrelloCard, error) {
 
 	} else {
 		q := u.Query()
-		q.Set("key", "")
-		q.Set("token", "")
+		q.Set("key", key)
+		q.Set("token", token)
 		q.Set("query", "label:changelog")
 		q.Set("cards_limit", "10")
 		q.Set("modelTypes", "cards")
@@ -59,8 +78,43 @@ func GetCards() ([]TrelloCard, error) {
 	}
 }
 
+func GetCardActions(id string) ([]TrelloAction, error) {
+	u, e := url.Parse(trello_api + "/cards/" + id + "/actions")
+	if e != nil {
+		return nil, e
+	} else {
+		q := u.Query()
+		q.Set("key", key)
+		q.Set("token", token)
+		u.RawQuery = q.Encode()
+		resp, err := http.Get(u.String())
+		if err != nil {
+			return nil, err
+		} else {
+			defer resp.Body.Close()
+			content, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return nil, err
+			} else {
+				result, err := unmarshallTrelloActions(content)
+				if err != nil {
+					return nil, nil
+				} else {
+					return result, nil
+				}
+			}
+		}
+	}
+}
+
 func unmarshallTrelloSearch(content []byte) (TrelloSearchResult, error) {
 	var result TrelloSearchResult
+	err := json.Unmarshal(content, &result)
+	return result, err
+}
+
+func unmarshallTrelloActions(content []byte) ([]TrelloAction, error) {
+	var result []TrelloAction
 	err := json.Unmarshal(content, &result)
 	return result, err
 }
--- main.go
+++ main.go
@@ -10,8 +10,20 @@ func main() {
 		panic(err)
 	} else {
 		for i := 0; i < len(cards); i++ {
-			println(cards[i].Name)
-			println(cards[i].DateLastActivity.String())
+			card := cards[i]
+			println(card.Name)
+			trelloActions, e := api.GetCardActions(card.Id)
+			if e != nil {
+				panic(e)
+
+			} else {
+				for j := 0; j < len(trelloActions); j++ {
+					action := trelloActions[j]
+					println(action.Type + " " + action.Date.String() + " " + action.Data.ListBefore.Name + " -> " + action.Data.ListAfter.Name)
+				}
+			}
+			println()
 		}
 	}
+
 }
